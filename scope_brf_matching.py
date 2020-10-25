e05a4a886a7581e9ed55eda1e1117d447e595214
Alex Yen <alyen@ucsd.edu>
2020-10-25 16:30:46 -0700

    something wrong with cross corr method

--- scope_brf_matching.py
+++ scope_brf_matching.py
@@ -4,15 +4,26 @@ import scipy.signal as ss
 eiko_cfl_13w = r'C:\Users\alexy\OneDrive\Documents\STIMA\bulb_database\csv_files\eiko_cfl_13w'
 eiko_cfl_23w = r'C:\Users\alexy\OneDrive\Documents\STIMA\bulb_database\csv_files\eiko_cfl_23w'
 philips_cfl_13w = r'C:\Users\alexy\OneDrive\Documents\STIMA\bulb_database\csv_files\philips_cfl_13w'
+eiko_incandescent_100w = r'C:\Users\alexy\OneDrive\Documents\STIMA\bulb_database\csv_files\eiko_incandescent_100w'
 
 eiko_cfl_13w_0_path = eiko_cfl_13w + '\\' + 'waveform_0.csv' #figure out some automated way for this
 eiko_cfl_13w_9_path = eiko_cfl_13w + '\\' + 'waveform_9.csv'
 eiko_cfl_23w_0_path = eiko_cfl_23w + '\\' + 'waveform_0.csv'
 philips_cfl_13w_path = philips_cfl_13w + '\\' + 'waveform_0.csv'
+eiko_incan_100w_path = eiko_incandescent_100w + '\\' + 'waveform_0.csv'
 #is there a fast/efficient way to load all the data so that I can just pick and choose without having to
 #hardcode path in?
 
 savgol_window = 31
+
+class scope_brf():
+    def __init__(self, csv_path):
+        brf_data = np.genfromtxt(eiko_cfl_13w_0_path, delimiter = ',')
+        brf_data = brf_data[1:len(brf_data)] #removing first row
+        time, brf, voltage = np.hsplit(brf_data, len(brf_data[0]))
+        self.time = time
+        self.brf = brf
+        self.voltage = voltage
 
 def show_plot(brf_data):
     plt.plot(brf_data)
@@ -31,8 +42,9 @@ def normalize_brf(brf):
 #hstack, smooth, normalize
 def process_brf_0(brf):
     brf = np.hstack(brf)
-    smoothed = savgol_window(brf)
-    normalized = normalize_brf(smoothed)
+    normalized = normalize_brf(brf)
+    # smoothed = savgol(brf)
+    # normalized = normalize_brf(smoothed)
     return normalized
 
 def map_in_to_out(waveform, in_min, in_max, out_min, out_max):
@@ -46,33 +58,24 @@ def map_in_to_out(waveform, in_min, in_max, out_min, out_max):
 def cross_corr(brf_1, brf_2):
     # correlate = ss.correlate(brf_1, brf_2, mode = 'full')/len(brf_1)
     correlate = np.correlate(brf_1, brf_2, mode = 'full')/len(brf_1)
-    show_plot(correlate)
     correlate = map_in_to_out(correlate, -0.5, 0.5, 0, 1) #maps to 0 and 1
     show_plot(correlate)
     max = round(np.amax(np.array(correlate)), 6)
     return max
-
-#implement some filtering/window averaging?
-class scope_brf():
-    def __init__(self, csv_path):
-        brf_data = np.genfromtxt(eiko_cfl_13w_0_path, delimiter = ',')
-        brf_data = brf_data[1:len(brf_data)] #removing first row
-        time, brf, voltage = np.hsplit(brf_data, len(brf_data[0]))
-        self.time = time
-        self.brf = brf
-        self.voltage = voltage
 
 if __name__ == "__main__":
     eiko_cfl_13w_0 = scope_brf(eiko_cfl_13w_0_path) #waveform_0
     eiko_cfl_13w_9 = scope_brf(eiko_cfl_13w_9_path)
     eiko_cfl_23w_0 = scope_brf(eiko_cfl_23w_0_path)
     philips_cfl_13w_0 = scope_brf(philips_cfl_13w_path)
+    eiko_incan_100w_0 = scope_brf(eiko_incan_100w_path)
 
     eiko_cfl_13w_0_brf = process_brf_0(eiko_cfl_13w_0.brf)
     eiko_cfl_13w_9_brf = process_brf_0(eiko_cfl_13w_9.brf)
     eiko_cfl_23w_0_brf = process_brf_0(eiko_cfl_23w_0.brf)
     philips_cfl_13w_0_brf = process_brf_0(philips_cfl_13w_0.brf)
-    
+    eiko_incan_100w_0_brf = process_brf_0(eiko_incan_100w_0.brf)
+
     #eiko cfl 13w 0 with 9
     cc1 = cross_corr(eiko_cfl_13w_0_brf, eiko_cfl_13w_9_brf)
 
@@ -81,3 +84,11 @@ if __name__ == "__main__":
 
     #eiko 13w with philips 13w
     cc3 = cross_corr(eiko_cfl_13w_0_brf, philips_cfl_13w_0_brf)
+
+    #eiko cfl 13w with philips incan 100w
+    cc4 = cross_corr(eiko_cfl_13w_0_brf, eiko_incan_100w_0_brf)
+
+    print(cc1)
+    print(cc2)
+    print(cc3)
+    print(cc4)
