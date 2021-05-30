acf60c7ab4cc6b87f48255525c8bd28e39f18eba
Alex Yen <alyen@ucsd.edu>
2021-05-29 18:05:48 -0700

    uploading matrix_calculations.py

--- Location Analysis/cv2_camera_calibration.py
+++ Location Analysis/cv2_camera_calibration.py
@@ -1,6 +1,11 @@
+import os
 import numpy as np
 import cv2
 import glob
+import time
+
+image_path = r'C:\Users\alexy\OneDrive\Documents\STIMA\Images\Rectification\calibration_1'
+current_dir = os.getcwd()
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -13,17 +18,30 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
+os.chdir(image_path)
 images = glob.glob('*.jpg')
+num_images = len(images)
+image_counter = 1
+num_pics_with_corners = 0
 
 for fname in images:
+    print(f'Working on {image_counter} / {num_images}')
+    
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
+    # print("findChessboardCorners benchmark:")
+    # tic = time.time()
     # Find the chess board corners
     ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    # toc = time.time() - tic
+    # print(f'findChessboardCorners took {toc} seconds')
 
-    # If found, add obje    ct points, image points (after refining them)
+    # If found, add object points, image points (after refining them)
     if ret == True:
+        print(fname)
+        num_pics_with_corners += 1
+        # print(f'Number of Pictures with Corners Found: {num_pics_with_corners}')
         objpoints.append(objp)
 
         corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
@@ -31,9 +49,26 @@ for fname in images:
 
         # Draw and display the corners
         img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
+        cv2.imwrite('calibration_results/corners_' + fname, img)
+    image_counter += 1
 
-cv2.destroyAllWindows()
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
 
-print(imgpoints)
+image_path_2 = r'C:\Users\alexy\OneDrive\Documents\STIMA\Images\Rectification\Rectification_Test'
+os.chdir(image_path_2)
+
+img = cv2.imread('left_0.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+
+print(mtx)
+print(dist)
+print(newcameramtx)
+
+# undistort
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+# crop the image
+x,y,w,h = roi
+dst = dst[y:y+h, x:x+w]
+cv2.imwrite('calibresult.png',dst)
---
+++ Location Analysis/matrix_calculations.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+def calc_camera_calibration_matrix(image_width, image_height, pixel_width, pixel_height, focal_length):
+    alpha_x = focal_length / pixel_width
+    alpha_y = focal_length / pixel_height
+
+    x0 = (image_width - 1) / 2
+    y0 = (image_height - 1) / 2
+
+    K = np.zeros((3,3))
+
+    K[0][0] = alpha_x
+    K[0][1] = 0
+    K[0][2] = x0
+    K[1][0] = 0
+    K[1][1] = alpha_y
+    K[1][2] = y0
+    K[2][0] = 0
+    K[2][1] = 0
+    K[2][2] =  1
+
+    return K
+
+if __name__ == '__main__':
+    # pixel_1; focal_length and pixel_width/pixel_height need to be in same units
+    pix1_image_width = 3044
+    pix1_image_height = 4048
+    pix1_sensor_width = 4.72 #in mm
+    pix1_sensor_height = 6.27 #in mm
+    pix1_pixel_width = pix1_sensor_width / pix1_image_width * 1000      #units: um/pixel
+    pix1_pixel_height = pix1_sensor_height / pix1_image_height * 1000   #units: um/pixel
+    pix1_focal_length = 4.67 * 1000                                     #units: um/pixel
+
+    # pixel_2
+    pix2_image_width = 3032
+    pix2_image_height = 4032
+    pix2_sensor_width = 4.24 #in mm
+    pix2_sensor_height = 5.64 #in mm
+    pix2_pixel_width = pix2_sensor_width / pix2_image_width * 1000      #units: um/pixel
+    pix2_pixel_height = pix2_sensor_height / pix2_image_height * 1000   #units: um/pixel
+    pix2_focal_length = 4.4589 * 1000                                   #units: um/pixel
+
+    K1 = calc_camera_calibration_matrix(pix1_image_width, pix1_image_height, pix1_pixel_width, pix1_pixel_height, pix1_focal_length)
+    K2 = calc_camera_calibration_matrix(pix2_image_width, pix2_image_height, pix2_pixel_width, pix2_pixel_height, pix2_focal_length)
+
+    print(K1)
+
+    print()
+
+    print(K2)
