008f4eb6f9396bc697b229c9f6e7963f131595a8
alwyen <alyen@ucsd.edu>
2024-05-27 11:30:27 -0700

    fixed up stereo_image_resize.py

--- calibration_code/stereo_image_resize.py
+++ calibration_code/stereo_image_resize.py
@@ -3,18 +3,34 @@ import os
 
 height = 960
 width = 2560
-#HOME = os.path.expanduser( '~' )
-HOME = os.path.expanduser( '~' ) + '/Projects/LightsCameraGrid/stereopi-setup/calibration'
+
+def get_max_num_images(home_path):
+    img_list = os.listdir(home_path)
+    count_list = list()
+    for img in img_list:
+        img_name = img.split('.')[0]
+        img_count = int(img_name.split('_')[-1])
+        count_list.append(img_count)
+
+    return max(count_list)
+
+
+HOME = os.path.join(os.sep, *os.getcwd().split(os.sep)[:-1], 'stereopi-setup', 'calibration')
 def main():
-    
-    for i in range(1,90):
+    num_images = get_max_num_images(HOME)
+    for i in range(1,num_images+1):
+        if i%10 == 0:
+            print(f'Image {i}/{num_images} done.')
         #if i in [56,57]: continue
         #print(HOME)
-        #path = '/Projects/LightsCameraGrid/Calibration Data/Stereo_Pi/calibration_images'
+        #path = '/Dropbox/LightsCameraGrid/Calibration Data/Stereo_Pi/calibration_images'
         img = cv2.imread(HOME + "/scene_{}x{}_{}.png".format(width, height, i))
 
         img1 = img[:, :int(img.shape[1]/2), :]
         img2 = img[:, int(img.shape[1]/2):, :]
+
+        if not os.path.exists('calibration_image_pairs'):
+            os.mkdir('calibration_image_pairs')
         if not os.path.exists('calibration_image_pairs/left_camera'):
             os.mkdir('calibration_image_pairs/left_camera')
         if not os.path.exists('calibration_image_pairs/right_camera'):
