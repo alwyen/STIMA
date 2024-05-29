24468335d1b6f736e08d60b560d9ec5ecdea790b
AnthonyQ619 <aquiroga2000@gmail.com>
2024-05-29 07:49:54 +0100

    Day Walking Dataset two

--- stereopi-setup/1_test.py
+++ stereopi-setup/1_test.py
@@ -32,7 +32,19 @@ import os
 from datetime import datetime
 import board
 import adafruit_bno055
+import sys
+import qwiic_titan_gps
 
+args = sys.argv
+dataNum = args[1]
+
+qwiicGPS = qwiic_titan_gps.QwiicTitanGps()
+
+if qwiicGPS.connected is False:
+    print("Could not connect to to the SparkFun GPS Unit. Double check thatit's wired correctly.", file=sys.stderr)
+    exit(1)
+
+qwiicGPS.begin()
 
 # Connecting Board
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -40,16 +52,22 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
 # Creating metadata file
-if (os.path.isdir("./imu_data") == False):
-    os.makedirs("./imu_data")
+metaf_dir = "./imu_data_{}".format(dataNum)
+if (os.path.isdir(metaf_dir) == False):
+    os.makedirs(metaf_dir)
 
 # Initialize CSV file for metadata
-csv_file = open("./imu_data/frame_data.csv", 'w')
+csv_file_name = metaf_dir + "/frame_data.csv"
+csv_file = open(csv_file_name, 'w')
 writer = csv.writer(csv_file)
-writer.writerows([['frame', 'X', 'Y', 'Z', 'q1', 'q2', 'q3', 'q4']])
+writer.writerows([['frame', 'X', 'Y', 'Z', 'q1', 'q2', 'q3', 'q4', 'lat', 'lon']])
 
 # Path for captured image
-path = './scenes/stereo_image'
+img_dir = "./scenes_{}".format(dataNum)
+if (os.path.isdir(img_dir) == False):
+    os.makedirs(img_dir)
+
+path = img_dir + '/stereo_image'
 font=cv2.FONT_HERSHEY_SIMPLEX # Fame Num. font
 
 
@@ -85,26 +103,34 @@ avgtime = 0
 # Capture frames from the camera
 for frame in camera.capture_continuous(capture, format="bgra", use_video_port=True): #, resize=(img_width,img_height)):
     counter+=1
-    t1 = datetime.now()
-    timediff = t1-t2
-    avgtime = avgtime + (timediff.total_seconds())
+    #t1 = datetime.now()
+    #timediff = t1-t2
+    #avgtime = avgtime + (timediff.total_seconds())
     frame_show = cv2.resize(frame, (int(img_width*0.25), int(img_height*0.25)))
     cv2.putText(frame_show, str(img_count), (50,50), font, 2.0, (0,255,0),4, cv2.LINE_AA)
     cv2.imshow("pair", frame_show)
     key = cv2.waitKey(1) & 0xFF
-    t2 = datetime.now()
+    #t2 = datetime.now()
     # if the `q` key was pressed, break from the loop and save last image
     if key == ord("p") :
-        avgtime = avgtime/counter
-        print ("Average time between frames: " + str(avgtime))
-        print ("Average FPS: " + str(1/avgtime))
-        print("Frame Number: " + str(img_count))
-        if (os.path.isdir("./scenes") == False):
-            os.makedirs("./scenes")
+        #avgtime = avgtime/counter
+        #print ("Average time between frames: " + str(avgtime))
+        #print ("Average FPS: " + str(1/avgtime))
+        print ("Frame Number: " + str(img_count))
+
+        # Getting Metadata for Images
         euler_data = [str(d) for d in sensor.euler]
         quaternion_data = [str(d) for d in sensor.quaternion]
-        line_csv = [['frame' + str(img_count)] + euler_data + quaternion_data]
+        gps_data = []
+        try:
+           if qwiicGPS.get_nmea_data() is True:
+               gpsData = [qwiicGPS.gnss_messages['Latitude'], qwiicGPS.gnss_messages['Longitude']]
+               gps_data = [str(d) for d in gpsData]
+        except: gps_data = [0.0, 0.0]
+
+        line_csv = [['frame' + str(img_count)] + euler_data + quaternion_data + gps_data]
         writer.writerows(line_csv)
+
         filename = path + str(img_count) + '.png'
         cv2.imwrite(filename, frame)
         img_count += 1
